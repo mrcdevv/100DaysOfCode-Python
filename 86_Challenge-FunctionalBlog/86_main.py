633dcc41015ba19b242ffa0ecd15481f22fbb12a
mrc <marcoemorales2015@gmail.com>
2023-02-18 23:37:09 -0300

    day 24 | session (update)

--- 86_Challenge-FunctionalBlog/86_main.py
+++ 86_Challenge-FunctionalBlog/86_main.py
@@ -1,1 +1,112 @@
 # Day 86 on Replit: "Challenge: Functional blog engine"
+
+from flask import Flask, redirect, request, session
+import mrcdb
+
+
+app = Flask(__name__, static_url_path='/static')
+app.secret_key = "JKLSJKLSDujiosjaklJKLSDJKLajfiodaujiO890AJklfjdlkJFIOJAM<IDK.j"
+
+
+try:
+    # Load the blogs on a dictionary
+    db = mrcdb.cvsTo2DDict("blogs.csv", "title")
+except:
+    db = {}
+
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    page = ""
+
+    # Load the main page
+    main = open("templates/blog.html", "r")
+    page = main.read()
+    main.close()
+
+    # If the admin is logged in
+    if session.get("logedIn"):
+        page = page.replace("{log}", "Logout")
+
+        # Load the "admin panel"
+        admin = open("templates/adminBlog.html", "r")
+        page += admin.read()
+        admin.close()
+
+        # If something is submited through the form
+        if request.method == 'POST':
+            form = request.form
+
+            # Check if the title already exist and if not...
+            if form["title"] not in db.keys():
+                # Load it in the db
+                db[form["title"]] = {
+                    "date": form["date"], "body": form["body"]}
+                # Save every single blog
+                mrcdb.dict2DToCsv("blogs.csv", "title", db)
+
+            else:
+                return redirect("/")
+
+    # If the admin isn't logged in
+    else:
+        page = page.replace("{log}", "Login")
+
+    # Check if exist at least one blog
+    if db.keys() != {}:
+
+        # Sort all the blogs from newest to oldest
+        keys = list(db.keys())
+        keys.reverse()
+
+        for key in keys:
+            # Load the basic template for each blog
+            posts = open("templates/post.html", "r")
+            page += posts.read()
+            posts.close()
+
+            page = page.replace("{title}", key)
+            page = page.replace("{date}", db[key]["date"])
+            page = page.replace("{text}", db[key]["body"])
+
+    return page
+
+
+@app.route("/login")
+def loginPage():
+
+    # If the admin is already logged in, don't let em login again
+    if session.get("logedIn"):
+        return redirect("/")
+    else:
+        page = ""
+
+        f = open("templates/login.html", "r")
+        page = f.read()
+        f.close()
+
+        return page
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    form = request.form
+
+    # Check credentials inputted
+    if form["user"] == "admin" and form["password"] == "admin1":
+        session["logedIn"] = "admin"
+        return redirect("/")
+    else:
+        return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    if session.get("logedIn"):
+        session.clear()
+        return redirect("/")
+    else:
+        return redirect("/login")
+
+
+app.run(host='0.0.0.0', port=81)
---
+++ 86_Challenge-FunctionalBlog/mrcdb.py
@@ -0,0 +1,78 @@
+import csv
+
+
+# Create a file
+def createDb(filename):
+    f = open(filename, "w")
+    f.close()
+
+
+# Convert a dictionary (nested in a list) to a csv file.
+def dictInListToCsv(filename, lst):
+    with open(filename, "w", newline="") as f:
+        header = lst[0].keys()
+        f = csv.DictWriter(f, header)
+        f.writeheader()
+
+        for row in lst:
+            f.writerow(row)
+
+
+# Convert a csv file to dictionary (nested in a list)
+def csvToDictInList(filename):
+    with open(filename, "r") as f:
+        f = csv.DictReader(f)
+        return list(f)
+
+
+# Convert a 2d dictionary to a csv file. The 'firstKeyName' parameter is used to represent the name that should have all the keys inside the dictionary,e.g.
+# {"david" : {"password": 14564564561, "email": "david149@gmail.com"}, "marc" : {"password": 727198125, "email": "marc756@hotmail.com"}}. In this case, all the keys are names.
+def dict2DToCsv(filename, firstKeyName, dic):
+    # Get headers
+    header = [firstKeyName]
+    for key, value in dic.items():
+        for subKey in value:
+            if subKey not in header:
+                header.append(subKey)
+
+    # Write and create the csv file
+    with open(filename, "w", newline="") as f:
+        f = csv.DictWriter(f, header)
+        f.writeheader()
+        row = {}
+        for key, value in dic.items():
+            row[firstKeyName] = key
+            for subKey, subValue in value.items():
+                row[subKey] = subValue
+            f.writerow(row)
+
+
+# Convert a csv file to a 2d dictionary
+def cvsTo2DDict(filename, firstKeyName):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        records = {}
+        for row in reader:
+            firstValue = row[firstKeyName]
+            innerDict = {}
+            for key in row.keys():
+                if key != firstKeyName:
+                    innerDict[key] = row[key]
+            records[firstValue] = innerDict
+        return records
+
+
+# Convert 1d dictionary to a csv file
+def dictToCsv(filename, dct):
+    with open(filename, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=dct.keys())
+        writer.writeheader()
+        writer.writerow(dct)
+
+
+# Convert a csv file to a 1d dictionary
+def csvToDict(filename):
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            return row
